3d8c74f2991311eb20625e258640c20568ef4d46
Yoshiro MIHIRA <sanpei@sanpei.org>
2021-08-14 08:39:44 +0900

    change NHK data source

--- covid19jp/covid19jp.js
+++ covid19jp/covid19jp.js
@@ -30,6 +30,57 @@ var prefTable = [];
 const maxFiles = 10;
 const loadingFilesElement = document.getElementById("loadingFiles");
 
+const prefJp2EnTable = {
+    "北海道": "Hokkaido",
+    "青森県": "Aomori",
+    "岩手県": "Iwate",
+    "宮城県": "Miyagi",
+    "秋田県": "Akita",
+    "山形県": "Yamagata",
+    "福島県": "Fukushima",
+    "茨城県": "Ibaraki",
+    "栃木県": "Tochigi",
+    "群馬県": "Gunma",
+    "埼玉県": "Saitama",
+    "千葉県": "Chiba",
+    "東京都": "Tokyo",
+    "神奈川県": "Kanagawa",
+    "新潟県": "Niigata",
+    "富山県": "Toyama",
+    "石川県": "Ishikawa",
+    "福井県": "Fukui",
+    "山梨県": "Yamanashi",
+    "長野県": "Nagano",
+    "岐阜県": "Gifu",
+    "静岡県": "Shizuoka",
+    "愛知県": "Aichi",
+    "三重県": "Mie",
+    "滋賀県": "Shiga",
+    "京都府": "Kyoto",
+    "大阪府": "Osaka",
+    "兵庫県": "Hyogo",
+    "奈良県": "Nara",
+    "和歌山県": "Wakayama",
+    "鳥取県": "Tottori",
+    "島根県": "Shimane",
+    "岡山県": "Okayama",
+    "広島県": "Hiroshima",
+    "山口県": "Yamaguchi",
+    "徳島県": "Tokushima",
+    "香川県": "Kagawa",
+    "愛媛県": "Ehime",
+    "高知県": "Kochi",
+    "福岡県": "Fukuoka",
+    "佐賀県": "Saga",
+    "長崎県": "Nagasaki",
+    "熊本県": "Kumamoto",
+    "大分県": "Oita",
+    "宮崎県": "Miyazaki",
+    "鹿児島県": "Kagoshima",
+    "沖縄県": "Okinawa",
+};
+
+
 const colorTable = [
 //    "red",
 //    "yellow",
@@ -1163,25 +1214,6 @@ function reformatToyoKeizaiData2CSSEGISandData(tdata, pref, i, type) {
     return a;
 }
 
-function reformatNHKData2CSSEGISandData(tdata, pref, a) {
-    let s = 0;
-    for (let l = 0; l < a.length; l++) {
-	a[l] = parseInt(a[l]) + s;
-	s = a[l];
-    }
-    const targetStartDay = tdata["category"][1];
-    const offsetdays = calculateOffsetDays(targetStartDay,
-					   dataStartDay);
-    for (let j = 1; j < offsetdays; j++) {
-	a.splice(0, 0, 0);
-    }
-    a.splice(0, 0, 0);
-    a.splice(0, 0, 0);
-    a.splice(0, 0, 0);
-    a.splice(0, 0, pref);
-    return a;
-}
-
 function parseToyoKeizaiData(data) {
     const tdata = JSON.parse(data);
     for (let j in tdata["prefectures-map"]) {
@@ -1195,9 +1227,41 @@ function parseToyoKeizaiData(data) {
     }
 }
 
-function parseNHKData(data) {
-    NHKOrgData = data;
-}
+function parseNHKData(str) {
+    let targetStartDay = "";
+    let offsetdays;
+    let lines = str.split("\n");
+    let s = 0;
+    let a = [];
+    let pref = "";
+    let prev_pref = "";
+    for (let i = 0; i < lines.length; ++i) {
+	const cells = lines[i].split(",");
+	if (cells == undefined || cells[0] == "") {
+	    break;
+	}
+	if (cells[0] == "日付") {
+	    continue;
+	}
+	if (targetStartDay == "") {
+	    targetStartDay = cells[0];
+	    offsetdays = calculateOffsetDays(targetStartDay,
+					     dataStartDay);
+	}
+	pref = prefJp2EnTable[cells[2]];
+	if (prev_pref != pref && a != []) {
+	    dataCasesNHK.push(a);
+	    prev_pref = pref;
+	    a = [pref, "Japan", 0, 0];
+	    for (let j = 1; j <= offsetdays; j++) {
+		a.push(0);
+	    }
+	}
+	s = parseInt(cells[4]);
+	a.push(s);
+    }
+    dataCasesNHK.push(a);
+};
 
 
 function parseTokyo(str) {
@@ -1341,7 +1405,7 @@ const urlGlobalDeath = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-1
 const urlGlobalRecoverd = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv';
 
 const urlToyoKeizai = 'https://raw.githubusercontent.com/sanpei3/toyokeizai-covid19jp/main/data.json';
-const urlNHK = 'https://www3.nhk.or.jp/news/special/coronavirus/data/47newpatients-data.json';
+const urlNHK = 'https://www3.nhk.or.jp/n-data/opendata/coronavirus/nhk_news_covid19_prefectures_daily_data.csv';
 
 const urlTokyoConfirmed = 'https://oku.edu.mie-u.ac.jp/~okumura/python/data/COVID-tokyo.csv';
 
@@ -1395,15 +1459,6 @@ async function main() {
 	dataCases = dataCasesJAG;
 	updateStartDay();
     }).then(results => {
-	const tdataNHK = JSON.parse(NHKOrgData);
-	for (let j in tdataNHK["data47"]) {
-	    const p = tdataNHK["data47"][j];
-	    const pref = prefTable[p["name"]];
-	    if (pref != undefined) {
-		const i = p["data"];
-		dataCasesNHK.push(reformatNHKData2CSSEGISandData(tdataNHK, pref, i));
-	    }
-	}
 	initialize();
     }).then(results => {
 	let loadingId = document.getElementById("loading");
